e599931e1d8317af2a8fd5d764a86a2a745bb712
Ruey <rueyce@gmail.com>
2019-05-26 17:09:28 +0800

    Unified checkGame and Coloring

--- src/App.js
+++ src/App.js
@@ -17,11 +17,9 @@ class App extends React.Component {
       opponentSymbol: '',
       currentPlayer: 'O',
       gameStarted: false,
+      findingGame: false,
       playerOne: '',
       playerTwo: '',
-      findingGame: false,
-      creatingGame: false,
-      findingGameFailed: false,
       board: ['','','','','','','','',''],
       hover: ['','','','','','','','',''],
     }
@@ -105,36 +103,30 @@ class App extends React.Component {
   isEqual = (x,y,z) => { if ((x !== '') && (x === y) && (x === z)) { return true } }
 
   checkGame = () => {
-    const score = this.state.board
-    if (this.isEqual(score[0],score[1],score[2])) { return score[0] }
-    else if (this.isEqual(score[0],score[3],score[6])) { return score[0] }
-    else if (this.isEqual(score[0],score[4],score[8])) { return score[0] }
-    else if (this.isEqual(score[1],score[4],score[7])) { return score[1] }
-    else if (this.isEqual(score[2],score[4],score[6])) { return score[2] }
-    else if (this.isEqual(score[2],score[5],score[8])) { return score[2] }
-    else if (this.isEqual(score[3],score[4],score[5])) { return score[3] }
-    else if (this.isEqual(score[6],score[7],score[8])) { return score[6] }
-    else if (this.checkDraw() === true) { return 'draw' }
-    else return 'ongoing'
-  }
-
-  testForColoring = () => {
-    const score = this.state.board
-    if (this.isEqual(score[0],score[1],score[2])) { return [0,1,2] }
-    else if (this.isEqual(score[0],score[3],score[6])) { return [0,3,6] }
-    else if (this.isEqual(score[0],score[4],score[8])) { return [0,4,8] }
-    else if (this.isEqual(score[1],score[4],score[7])) { return [1,4,7] }
-    else if (this.isEqual(score[2],score[4],score[6])) { return [2,4,6] }
-    else if (this.isEqual(score[2],score[5],score[8])) { return [2,5,8] }
-    else if (this.isEqual(score[3],score[4],score[5])) { return [3,4,5] }
-    else if (this.isEqual(score[6],score[7],score[8])) { return [6,7,8] }
-    else if (this.checkDraw() === true) { return 'draw' }
-    else return 'ongoing'
+    const board = this.state.board
+    const winCondition = [
+      [0,1,2],
+      [0,3,6],
+      [0,4,8],
+      [1,4,7],
+      [2,4,6],
+      [2,5,8],
+      [3,4,5],
+      [6,7,8],
+    ]
+    for(let i = 0; i < winCondition.length; i++) {
+      const index = winCondition[i]
+      if (this.isEqual(board[index[0]],board[index[1]],board[index[2]])) {
+        return index
+      }
+    }
+    if (this.checkDraw() === true) { return 'draw' }
+    return 'ongoing'
   }
 
   checkDraw = () => {
-    let score = this.state.board
-    for (let i = 0; i < score.length; i++) { if (score[i] === '') { return false } }
+    const board = this.state.board
+    for (let i = 0; i < board.length; i++) { if (board[i] === '') { return false } }
     return true
   }
 
@@ -245,12 +237,12 @@ class App extends React.Component {
       let spanClass = "lead display-3 font-weight-bold "
       if (this.state.board[index] === 'O') { spanClass += "text-playerone " }
       else { spanClass += "text-playertwo "}
-      if (this.testForColoring().indexOf(index) !== -1) { spanClass += "winning-combo-bois " }
+      if (this.checkGame().indexOf(index) !== -1) { spanClass += "winning-combo-bois " }
 
       let divClass = "game-tile "
       if (index !== 6 && index !== 7 && index !== 8) { divClass += "border-tac-bottom " }
       if (index !== 2 && index !== 5 && index !== 8) { divClass += "border-tac-right " }
-      if (this.testForColoring().indexOf(index) !== -1) { divClass += "winning-combo-lads " }
+      if (this.checkGame().indexOf(index) !== -1) { divClass += "winning-combo-lads " }
 
       return (
         <div 
@@ -272,85 +264,59 @@ class App extends React.Component {
       <Row>
         <Col md="12" className="bg-dark d-flex py-4 justify-content-center align-items-center flex-column text-center cursor-default top-status-minh">
 
-        { !this.state.myUsername ? 
-          <React.Fragment>
-            <img src={loading} alt="Mount-Loader" width="75" height="75"></img>
-            <p className="text-light lead my-0" style={{fontSize:22}}>Contacting server...</p>
-            {/* <p className="lead my-0 text-success font-weight-bold" style={{fontSize:25}}>[O] Trodden Coil (You)</p> */}
-            {/* <p className="lead my-0 py-0 text-light">vs</p> */}
-            {/* <p className="lead my-0 text-primary font-weight-bold" style={{fontSize:25}}>[X] Unstitched Scent</p> */}
+          { !this.state.myUsername ? 
+            <React.Fragment>
+              <img src={loading} alt="Mount-Loader" width="75" height="75"></img>
+              <p className="text-light lead my-0" style={{fontSize:22}}>Contacting server...</p>
+            </React.Fragment>
+            : null }
 
-          </React.Fragment>
-          : null }
+          { !this.state.findingGame && !this.state.gameStarted && this.state.myUsername ? 
+            <React.Fragment>
+              <p className="text-light lead my-0" style={{fontSize:22}}>Multiplayer Pepega Toe</p>
+              <p className="text-light lead my-0" style={{fontSize:22}}>Hello, {this.state.myUsername}.</p>
+            </React.Fragment>
+            : null }
 
-        { !this.state.creatingGame && !this.state.findingGame && !this.state.findingGameFailed && !this.state.gameStarted && this.state.myUsername ? 
-          <React.Fragment>
-            {/* <p className="lead my-0 text-success font-weight-bold" style={{fontSize:25}}>[O] Trodden Coil (You)</p>
-            <p className="lead my-0 text-primary font-weight-bold" style={{fontSize:25}}>[X] Unstitched Scent</p> */}
-            <p className="text-light lead my-0" style={{fontSize:22}}>Multiplayer Pepega Toe</p>
-            <p className="text-light lead my-0" style={{fontSize:22}}>Hello, {this.state.myUsername}.</p>
-          </React.Fragment>
-          : null }
+          { this.state.findingGame ? 
+            <React.Fragment>
+              <img src={loading} alt="Find-Loader" width="75" height="75"></img>
+              <p className="text-light lead my-0" style={{fontSize:22}}>Looking for players...</p>
+            </React.Fragment>
+            : null }
 
-        { this.state.creatingGame ? 
-          <React.Fragment>
-            <img src={loading} alt="Create-Loader" width="75" height="75"></img>
-            {/* <p className="text-light lead my-0">No games found, hosting now.</p>
-            <p className="text-light lead my-0">Waiting for players...</p> */}
-            <p className="text-light lead my-0">Hosting a room...</p>
-          </React.Fragment>
-          : null }
+          { this.state.gameStarted ?
+            <React.Fragment>
 
-        { this.state.findingGame ? 
-          <React.Fragment>
-            <img src={loading} alt="Find-Loader" width="75" height="75"></img>
-            {/* <p className="text-light lead my-0">Searching for existing hosts...</p> */}
-            <p className="text-light lead my-0" style={{fontSize:22}}>Looking for players...</p>
-          </React.Fragment>
-          : null }
+              <p className="lead my-0 text-success" style={{fontSize:25}}><span className="font-weight-bold">[O]</span> {this.state.playerOne} {this.state.playerOne === this.state.myUsername && this.state.myUsername !== '' ? '(You)' : null}</p>
+              <p className="lead mt-0 mb-2 text-primary" style={{fontSize:25}}><span className="font-weight-bold">[X]</span> {this.state.playerTwo} {this.state.playerTwo === this.state.myUsername && this.state.myUsername !== '' ? '(You)' : null}</p>
 
-        { this.state.findingGameFailed ? 
-          <React.Fragment>
-            <p className="text-light lead my-0">Looks like no one's hosting.</p>
-            <p className="text-light lead my-0">Host one yourself!</p>
-          </React.Fragment>
-          : null }
+              { this.state.board[this.checkGame()[0]] === this.state.mySymbol ? 
+                <p className="lead text-light text-center my-0" style={{fontSize:22}}>You won!</p> 
+                : null }
 
-        { this.state.gameStarted ?
-          <React.Fragment>
-            <p className="lead my-0 text-success" style={{fontSize:25}}><span className="font-weight-bold">[O]</span> {this.state.playerOne} {this.state.playerOne === this.state.myUsername && this.state.myUsername !== '' ? '(You)' : null}</p>
-            <p className="lead mt-0 mb-2 text-primary" style={{fontSize:25}}><span className="font-weight-bold">[X]</span> {this.state.playerTwo} {this.state.playerTwo === this.state.myUsername && this.state.myUsername !== '' ? '(You)' : null}</p>
+              { this.state.board[this.checkGame()[0]] === this.state.opponentSymbol ? 
+                <p className="lead text-light text-center my-0" style={{fontSize:22}}>You lost!</p>
+                : null } 
 
-            {/* <p className="text-light lead my-0">Player One (O): {this.state.playerOne} {this.state.playerOne === this.state.myUsername && this.state.myUsername !== '' ? '(You)' : null}</p>
-            <p className="text-light lead mt-0 mb-3">Player Two (X): {this.state.playerTwo} {this.state.playerTwo === this.state.myUsername && this.state.myUsername !== '' ? '(You)' : null}</p> */}
+              { this.checkGame() === 'draw' ? 
+                <p className="lead text-light text-center my-0" style={{fontSize:22}}>It's a draw!</p> 
+                : null }
+              { this.checkGame() === 'ongoing' && this.state.gameStarted === true ? 
+                <p className="lead text-light text-center my-0" style={{fontSize:22}}>
+                  { this.state.currentPlayer === this.state.mySymbol ? 
+                    "It's your turn." : 
+                    `Waiting for ${this.state.myOpponent}'s move.`}
+                </p> : null }
 
-            { this.checkGame() === this.state.mySymbol ? 
-              <p className="lead text-light text-center my-0" style={{fontSize:22}}>You won!</p> 
-              : null }
-
-            { this.checkGame() === this.state.opponentSymbol ? 
-              <p className="lead text-light text-center my-0" style={{fontSize:22}}>You lost!</p>
-              : null } 
-
-            { this.checkGame() === 'draw' ? 
-              <p className="lead text-light text-center my-0" style={{fontSize:22}}>It's a draw!</p> 
-              : null }
-            { this.checkGame() === 'ongoing' && this.state.gameStarted === true ? 
-              <p className="lead text-light text-center my-0" style={{fontSize:22}}>
-                { this.state.currentPlayer === this.state.mySymbol ? 
-                  "It's your turn." : 
-                  `Waiting for ${this.state.myOpponent}'s move.`}
-              </p> : null }
-          </React.Fragment>
-        : null }
+            </React.Fragment> : null }
 
         </Col>  
 
         <Col md="12" className="">
           <div>
             <div className="mt-3 pt-2 text-center">
-              {/* <Button color="success" onClick={this.handleCreate} disabled={!this.state.myUsername || this.state.creatingGame || this.state.findingGame} className="mt-2 mx-2">Host Match</Button> */}
-              <Button color="primary" onClick={this.handleConnect} disabled={!this.state.myUsername || this.state.findingGame || this.state.creatingGame || ((this.checkGame() === 'ongoing') && this.state.gameStarted)} className="mt-2 mx-2">Find a game</Button>
+              <Button color="primary" onClick={this.handleConnect} disabled={!this.state.myUsername || this.state.findingGame || ((this.checkGame() === 'ongoing') && this.state.gameStarted)} className="mt-2 mx-2">Find a game</Button>
             </div>
           </div>
           <div id="game-board">{boardComponents}</div>
